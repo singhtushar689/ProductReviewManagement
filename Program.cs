5627c3446419e7b27e625854dcf35b1757a55076
singhtushar689 <singhtushar689@gmail.com>
2023-01-18 15:23:40 +0530

    Created A list of Product

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ProcutReviewManagement
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management");
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("Id");
+            dataTable.Columns.Add("ProductName");
+
+            dataTable.Rows.Add("1")
+
+        }
+    }
+}
